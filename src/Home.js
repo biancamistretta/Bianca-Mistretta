70e4ff1aacfb2bc24e665399e59c6ea9a1cab910
biancamistretta <142269375+biancamistretta@users.noreply.github.com>
2024-03-18 18:54:18 -0400

    Create Home.js and add code

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section>
+            <h2>Welcome to My Portfolio</h2>
+            <p>My name is Bianca and I am studying Full Stack Engineering!</p>
+        </section>
+    );
+};
+
+export default Home;
