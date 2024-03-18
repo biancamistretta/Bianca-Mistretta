cd94c0798d26acb13fa7fc2fefd1592dbfee8a31
biancamistretta <142269375+biancamistretta@users.noreply.github.com>
2024-03-18 18:55:20 -0400

    Create experience.js and add respons later

---
+++ src/experience.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Experience = () => {
+    return (
+        <section>
+            <h2>Experience</h2>
+            <p>Company Name - Position Title</p>
+            <p>Date Range</p>
+            <p>Responsibilities:</p>
+            <ul>
+                <li>Responsibility 1</li>
+                <li>Responsibility 2</li>
+                {/* Add more responsibilities */}
+            </ul>
+        </section>
+    );
+};
+
+export default Experience;
