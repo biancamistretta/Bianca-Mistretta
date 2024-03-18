5c4d0578137855999de0c2baf318a91c63964bb8
biancamistretta <142269375+biancamistretta@users.noreply.github.com>
2024-03-18 18:55:01 -0400

    Create skills.js and add more later

---
+++ src/skills.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <section>
+            <h2>Skills</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                {/* Add more skills */}
+            </ul>
+        </section>
+    );
+};
+
+export default Skills;
